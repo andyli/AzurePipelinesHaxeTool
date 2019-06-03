db9f3a9e0410b837c0f26ccfb91a1012ddf64fef
Andy Li <andy@onthewings.net>
2019-06-03 18:43:49 +0800

    Install neko the way as haxe does. TODO: deal with loading libneko.

--- UseHaxeVersion/src/Main.hx
+++ UseHaxeVersion/src/Main.hx
@@ -6,7 +6,9 @@ import haxe.io.*;
 using StringTools;
 
 class Main {
+    static var nekoVersion(default, never):String = "2.2.0";
     static var platform(default, never):String = Os.platform();
+
     static function haxeUrl(version:String):String {
         var file = switch (platform) {
             case "win32":
@@ -19,6 +21,20 @@ class Main {
                 throw "unsupported platform: " + platform;
         }
         return 'https://haxe.org/website-content/downloads/${version}/downloads/${file}';
+    }
+
+    static function nekoUrl(version:String):String {
+        var file = switch (platform) {
+            case "win32":
+                'neko-${version}-win.zip';
+            case "darwin":
+                'neko-${version}-osx64.tar.gz';
+            case "linux":
+                'neko-${version}-linux64.tar.gz';
+            case _:
+                throw "unsupported platform: " + platform;
+        }
+        return 'https://github.com/HaxeFoundation/neko/releases/download/v${version.replace(".", "-")}/${file}';
     }
 
     static function getOnlySubDir(path:String):String {
@@ -48,29 +64,63 @@ class Main {
             });
     }
 
-    static function handleHaxeInstallPath(path:String):Void {
+    static function acquireNeko(version:String):Promise<String> {
+        var url = nekoUrl(version);
+        var fileName = Path.withoutDirectory(url);
+        return Tool.downloadTool(url)
+            .then(function(downloadPath:String) {
+                if (fileName.endsWith(".tar.gz"))
+                    return Tool.extractTar(downloadPath);
+                else if (fileName.endsWith(".zip"))
+                    return Tool.extractZip(downloadPath);
+                else
+                    throw 'No idea of how to handle $fileName';
+            })
+            .then(function(extPath:String) {
+                var subDir = getOnlySubDir(extPath);
+                return Tool.cacheDir(subDir, "neko", version);
+            });
+    }
+
+    static function handleHaxeInstallPath(path:String):Null<Void> {
         Tool.prependPath(path);
         Task.setVariable("HAXE_STD_PATH", Path.join([path, "std"]));
-        Task.setResult(TaskResult.Succeeded, "");
+        return null;
+    }
+
+    static function handleNekoInstallPath(path:String):Null<Void> {
+        Tool.prependPath(path);
+        Task.setVariable("NEKOPATH", path);
+        return null;
     }
 
     static function main() {
         try {
             var versionSpec = Task.getInput("versionSpec", true);
 
-            var installDir = null;
+            var nekoInstallDir:Promise<String> = switch (Tool.findLocalTool("neko", nekoVersion)) {
+                case null:
+                    acquireNeko(nekoVersion);
+                case p:
+                    Promise.resolve(p);
+            };
+            var haxeInstallDir:Promise<String> = switch (Tool.findLocalTool("haxe", versionSpec)) {
+                case null:
+                    acquireHaxe(versionSpec);
+                case p:
+                    Promise.resolve(p);
+            };
 
-            installDir = Tool.findLocalTool("Haxe", versionSpec);
-
-            if (installDir == null) {
-                acquireHaxe(versionSpec)
-                    .then(handleHaxeInstallPath)
-                    .catchError(function(e:js.Error){
-                        Task.setResult(TaskResult.Failed, e.message);
-                    });
-            } else {
-                handleHaxeInstallPath(installDir);
-            }
+            Promise.all([
+                nekoInstallDir.then(handleNekoInstallPath),
+                haxeInstallDir.then(handleHaxeInstallPath),
+            ])
+                .then(function(v) {
+                    Task.setResult(TaskResult.Succeeded, "");
+                })
+                .catchError(function(e:js.Error){
+                    Task.setResult(TaskResult.Failed, e.message);
+                });
         } catch (e:js.Error) {
             Task.setResult(TaskResult.Failed, e.message);
         }
